2d41e5171c1e6eb3a8b898edbe953e6585fd3d65
khanh3733 <nguyenduykhanhgaim@gmail.com>
2022-11-10 19:25:04 +0700

    upload

---
+++ Thực Chiến/wheather App/main.js
@@ -0,0 +1,13 @@
+const search = document.querySelector('.search');
+const city = document.querySelector('.city');
+const country = document.querySelector('.country');
+const value = document.querySelector('.value');
+const sortDesc = document.querySelector('.sort-desc');
+const visibility = document.querySelector('.visibility span');
+const wind = document.querySelector('.wind span');
+const sun = document.querySelector('.sun span');
+
+
+function changeWeatherUL(){
+    search.value.trim()
+}
